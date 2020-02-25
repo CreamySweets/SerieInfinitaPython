f66fc48a4c1c0ee48fa29a3fec489aa22d80a8de
CreamySweets <victorfc_1804@hotmail.com>
2020-02-25 14:36:26 -0600

    Cambio de máximo en el ciclo, de 1000 a 2000

--- Serie infinita.py
+++ Serie infinita.py
@@ -1,5 +1,5 @@
 input("Presiona cualquier tecla para empezar")
-for i in range (0, 1000+1):
+for i in range (0, 2000+1):
 	print(str(i) + " ", end="")
 	i = i+1
 input("\nPresiona cualquier tecla para salir")
